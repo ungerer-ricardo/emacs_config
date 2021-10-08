f34e5eb643248e71d2893e0a4d99aacad315db0d
Ricardo Ungerer <ungerer.ricardo@gmail.com>
2021-10-08 17:00:47 +0100

    Adding autocomplete inittial support

--- init.el
+++ init.el
@@ -14,6 +14,40 @@
 (setq rtags-autostart-diagnostics t)
 (rtags-enable-standard-keybindings)
 
+;; Autocompletion irony package
+(require 'irony)
+
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+;; -- irony - company integration
+(require 'company-irony)
+
+(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+(setq company-backends (delete 'company-semantic company-backends))
+(require 'company-irony-c-headers)
+(eval-after-load 'company
+  '(add-to-list
+    'company-backends '(company-irony-c-headers company-irony)))
+
+(setq company-idle-delay 0)
+(define-key c-mode-map [(tab)] 'company-complete)
+(define-key c++-mode-map [(tab)] 'company-complete)
+
+;; cmake integration
+(require 'cmake-ide)
+(cmake-ide-setup)
+
 ;; Disabling menu-bar
 (menu-bar-mode -1)
 
@@ -33,3 +67,16 @@
 	      tab-width 4
 	      )
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(cmake-ide company-irony-c-headers company-irony irony company-rtags)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
